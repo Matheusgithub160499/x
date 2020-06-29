6f261ca439361a070a21910545d200832c47a292
Matheusgithub160499 <63514537+Matheusgithub160499@users.noreply.github.com>
2020-06-29 17:44:39 -0300

    Create Voo.ts

---
+++ src/models/Voo.ts
@@ -0,0 +1,13 @@
+export interface Voo {
+    id: number;
+    destino: aeroporto;
+	partida: aeroporto;
+	usuarios: usuario[];
+	descricao: string;
+	custo_adulto: number;
+	custo_meia: number;
+	data_hora_de_ida_partida: String; 
+	data_hora_de_chegada_partida: String;
+	data_hora_de_ida_volta: String;
+	data_hora_de_chegada_volta: String;
+}
